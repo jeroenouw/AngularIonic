22bb16fedc4220cee2959d1a8e84d41cb47a8114
Jeroenouw <jeroenouw@hotmail.com>
2017-07-26 08:55:03 +0200

    Users page updated

--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { User } from '../../models/user.model';
+import { GithubUsersProvider } from '../../providers/github-users/github-users';
+
 @Component({
   selector: 'page-users',
   templateUrl: 'users.html',
 })
 export class UsersPage {
+  users: User[]
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private guProvider: GithubUsersProvider) {
+    this.guProvider.getUsers()
+      .subscribe(
+        users => { this.users = users; })
   }
-
+  
+  /*
   ionViewDidLoad() {
     console.log('ionViewDidLoad UsersPage');
-  }
+  }*/
 
 }
