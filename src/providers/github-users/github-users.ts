19058d09ac3268db95dc4c2749e06639b3de3187
Jeroenouw <jeroenouw@hotmail.com>
2017-07-26 08:38:28 +0200

    Github service updated

--- src/providers/github-users/github-users.ts
+++ src/providers/github-users/github-users.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
-
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+import { User } from '../../models/user.model';
 
 @Injectable()
 export class GithubUsersProvider {
@@ -10,7 +12,18 @@ export class GithubUsersProvider {
   baseUrl = 'https://api.github.com';
 
   constructor(public http: HttpClient) {
-    console.log('Hello GithubUsersProvider Provider');
   }
 
+  getUsers(): Observable<User[]> {
+    return this.http
+      .get(`${this.baseUrl}/users`)
+      .catch(this.handleError);
+  }  
+  
+  private handleError (error: any) {
+    const errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
 }
