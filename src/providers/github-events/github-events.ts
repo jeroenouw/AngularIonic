7a7f3df4099a9c76ba154c85c23ecb9d08d3a4dd
Jeroenouw <jeroenouw@hotmail.com>
2017-07-27 13:54:52 +0200

    HttpHeaders included

--- src/providers/github-events/github-events.ts
+++ src/providers/github-events/github-events.ts
@@ -1,26 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
 
 import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class GithubEventsProvider {
+  headers: HttpHeaders;  
+  header: any;
 
   baseUrl = 'https://api.github.com';
 
-  constructor(public http: HttpClient) {
+  constructor(public http: HttpClient) {   
+    this.headers = new HttpHeaders({ 'Content-Type': 'application/vnd.github.v3+json', 'Accept': 'q=0.8;application/json;q=0.9' });
+    this.header = ({ headers: this.headers });
   }
 
   getEvents(): Observable<any> {
     return this.http
-      .get(`${this.baseUrl}/events`)
+      .get(`${this.baseUrl}/events`, this.header)
       .catch(this.handleError);
   }  
 
   getEventDetails(login: any): Observable<any> {
     return this.http
-      .get(`${this.baseUrl}/users/${login}/events`)
+      .get(`${this.baseUrl}/users/${login}/events`, this.header)
       .catch(this.handleError);
   }
     
--- src/providers/github-users/github-users.ts
+++ src/providers/github-users/github-users.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
 
 import 'rxjs/add/operator/catch';
@@ -8,27 +8,31 @@ import { User } from '../../models/user.model';
 
 @Injectable()
 export class GithubUsersProvider {
+  headers: HttpHeaders;  
+  header: any;
 
   baseUrl = 'https://api.github.com';
 
-  constructor(public http: HttpClient) {
+  constructor(public http: HttpClient) {   
+    this.headers = new HttpHeaders({ 'Content-Type': 'application/vnd.github.v3+json', 'Accept': 'q=0.8;application/json;q=0.9' });
+    this.header = ({ headers: this.headers });
   }
 
   getUsers(): Observable<User[]> {
     return this.http
-      .get<User[]>(`${this.baseUrl}/users`)
+      .get<User[]>(`${this.baseUrl}/users`, this.header)
       .catch(this.handleError);
   }  
 
   getUserDetails(login: string): Observable<User> {
     return this.http
-      .get<User>(`${this.baseUrl}/users/${login}`)
+      .get<User>(`${this.baseUrl}/users/${login}`, this.header)
       .catch(this.handleError);
   }
 
   searchUsers(searchParam: string): Observable<User[]> {
     return this.http
-      .get<User[]>(`${this.baseUrl}/search/users?q=${searchParam}`)
+      .get<User[]>(`${this.baseUrl}/search/users?q=${searchParam}`, this.header)
       .catch(this.handleError);
   }
   
