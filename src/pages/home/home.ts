7afb202ddb9ceb54175c4d091c0114409fc96198
Jeroenouw <jeroenouw@hotmail.com>
2017-07-28 10:45:24 +0200

    Homepage slider added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AlertController , NavController, NavParams } from 'ionic-angular';
+import { AlertController, NavController, NavParams } from 'ionic-angular';
 
 import { UsersPage } from '../users/users';
 
@@ -8,10 +8,27 @@ import { UsersPage } from '../users/users';
   templateUrl: 'home.html',
 })
 export class HomePage {
+  slides = [
+    {
+      title: "Angular Ionic mobile application",
+      description: "This is a simple app with <b>REST API</b> and <b>observables</b> to get information from the <b>Github API</b>.",
+      image: "assets/ica-slidebox-img-1.png",
+    },
+    {
+      title: "What is Ionic?",
+      description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+      image: "assets/ica-slidebox-img-2.png",
+    },
+    {
+      title: "What is Angular?",
+      description: "<b>Angular</b> is a framework for building client applications in HTML and TypeScript that compiles to JavaScript.",
+      image: "assets/ica-slidebox-img-3.png",
+    }
+  ];
 
-  constructor(public navCtrl: NavController, 
-              public navParams: NavParams,
-              public alertCtrl: AlertController) {
+  constructor(public alertCtrl: AlertController,
+              public navCtrl: NavController, 
+              public navParams: NavParams) {
   }
 
   goTo() {
