ac30859d6db6776615a493bb8869ebd97d3fe6f9
Jeroenouw <jeroenouw@hotmail.com>
2017-07-28 10:07:49 +0200

    Orgs page and provider updated

--- src/pages/organisations/organisations.ts
+++ src/pages/organisations/organisations.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { GithubOrgsProvider } from '../../providers/github-orgs/github-orgs';
+
 @Component({
   selector: 'page-organisations',
   templateUrl: 'organisations.html',
 })
 export class OrganisationsPage {
+  orgs: any[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private goProvider: GithubOrgsProvider) 
+  {
+    this.goProvider.getOrgs()
+      .subscribe(orgs => { 
+        this.orgs = orgs;  
+      })
   }
-
 
 }
--- src/providers/github-orgs/github-orgs.ts
+++ src/providers/github-orgs/github-orgs.ts
@@ -1,12 +1,31 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Rx';
+
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class GithubOrgsProvider {
+  headers: HttpHeaders;  
+  header: any;
 
-  constructor(public http: Http) {
-    console.log('Hello GithubOrgsProvider Provider');
+  baseUrl = 'https://api.github.com';
+
+  constructor(public http: HttpClient) {   
+    this.headers = new HttpHeaders({ 'Content-Type': 'application/vnd.github.v3+json', 'Accept': 'q=0.8;application/json;q=0.9' });
+    this.header = ({ headers: this.headers });
   }
+  
+  getOrgs(): Observable<any[]> {
+    return this.http
+      .get(`${this.baseUrl}/organizations`, this.header)
+      .catch(this.handleError);
+  }  
 
+  private handleError (error: any) {
+    const errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
 }
